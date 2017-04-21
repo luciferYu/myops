5a975cfb8507e03ec43476d699fc4b03897fc9c6
zhiyi <yuzy@fruitday.com>
2017-04-21 15:53:47 +0800

    add  lock

--- mtl.py
+++ mtl.py
@@ -5,16 +5,24 @@ import time,threading
 
 #假定这是你的银行存款
 balance = 0
+lock = threading.Lock()
 
 def change_it(n):
     #先存后去，结果应该为0
     global balance
     balance = balance + n
     balance = balance - n
+    #print('%s thread %s'%(balance,threading.current_thread().name))
 
 def run_thread(n):
-    for i in range(100000):
-        change_it(n)
+    for i in range(1000000):
+        #先要获取锁：
+        lock.acquire()
+        try:
+            change_it(n)
+        finally:
+            #改完一定要释放锁
+            lock.release()
 
 t1 = threading.Thread(target=run_thread,args=(5,))
 t2 = threading.Thread(target=run_thread,args=(8,))
